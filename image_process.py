ca53871a7c226269baae6bee6c574c0a167e14a4
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-12-01 17:34:01 +0800

    Update image_process.py

--- image_process.py
+++ image_process.py
@@ -1,20 +1,37 @@
+"""
+Author: 何彥南 (yen-nan ho)
+Github: https://github.com/aaron1aaron2
+Email: aaron1aaron2@gmail.com
+Create Date: 2022.12.01
+Last Update: 2022.12.01
+Describe: 資料前處理、產生訓練資料(train、val、test)
+"""
 import os
-import math
-import zipfile
 import cv2
 import imghdr
+import random
+import argparse
 
+import warnings
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
-import numpy as np
-import matplotlib.pyplot as plot
+def get_args():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--config_path', type=str, default='configs/Basic.yaml')
 
-from random import randint
-from datetime import datetime
+    args = vars(parser.parse_args())
+    config = read_config(args['config_path'])
+    args.update(config)
+
+    return args
 
 root_path = 'rawdata'
+output_folder = 'data/sample200'
+sample_num_per_class = 200
+
 file_ls = [os.path.join(root_path, i) for i in os.listdir(root_path) if i.find('.zip') != -1]
 
-class_ls = [
+CLASS_ls = [
     'asparagus', 'bambooshoots', 'betel', 'broccoli', 'cauliflower',
     'chinesecabbage', 'chinesechives', 'custardapple', 'grape', 'greenhouse',
     'greenonion', 'kale', 'lemon', 'lettuce', 'litchi', 'longan', 'loofah',
@@ -27,7 +44,7 @@ class_ls = [
 # code_name_dt = {i:name for i, name in enumerate(class_ls)} 
 
 def get_class(label_code):    
-    return class_ls[label_code]
+    return CLASS_ls[label_code]
 
 def output_zip(path, image_ls):
     # 建立 ZIP 壓縮檔
@@ -36,6 +53,28 @@ def output_zip(path, image_ls):
         for n, i in image_ls:
             zf.writestr(n, i)
 
-for f in file_ls:
-    with zipfile.ZipFile(f, 'r') as zf:
-        images = zf.namelist()
+# def read_img(path, image_ls):
+#     images_sample = random.shuffle(images)[:sample_num_per_class]
+
+img_tag_Y_df_dict = {}
+img_tag_Y_list_dict = {}
+
+def img_tag_Y_list(plant_class):
+    """
+    get img_name list w/ tag in eacg plant_class
+    """
+    img_tag_Y_list_df = img_tag_Y[img_tag_Y["Img"].isin(img_name_plant_class[plant_class])]
+    img_tag_Y_df_dict[plant_class] = img_tag_Y_list_df
+    img_tag_Y_list_dict[plant_class] = img_tag_Y_list_df["Img"].tolist()
+
+for plant_class in plant_class_list:
+    img_tag_Y_list(plant_class)
+    
+for k, v in img_tag_Y_list_dict.items():
+    print("{}:  Numbers of crop_img = {}".format(k, len(v)))
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
