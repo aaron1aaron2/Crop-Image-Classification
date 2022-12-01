6da6b309883db552f3591ddf5af8efffa4b17b0a
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-12-01 20:29:58 +0800

    Update image_process.py

--- scripts/image_process.py
+++ scripts/image_process.py
@@ -8,13 +8,16 @@ Describe: 資料前處理、產生訓練資料(train、val、test)
 """
 import os
 import cv2
+import glob
+import tqdm
 import imghdr
 import random
 import argparse
 
-from IPython import embed
 
-CLASS_ls = []
+import pandas as pd
+
+from IPython import embed
 
 def get_args():
     parser = argparse.ArgumentParser(add_help=False)
@@ -24,13 +27,9 @@ def get_args():
     parser.add_argument('--class_list_path', type=str, default='data/class_ls.txt')
 
     parser.add_argument('--img_coordinate_path', type=str, default='data/tag_locCoor.csv')
-    parser.add_argument('--coordinate_X', type=str, default='target_x', 
-                            help='The coordinate name of X in the coordinate file')
-    parser.add_argument('--coordinate_Y', type=str, default='target_y', 
-                            help='The coordinate name of Y in the coordinate file')
 
     # output
-    parser.add_argument('--output_folder', type=str, default='data/sample200')
+    parser.add_argument('--output_folder', type=str, default='data/sample_baseline')
 
     # parameters
     parser.add_argument('--sample_num_per_class', type=int, default=200)
@@ -38,28 +37,13 @@ def get_args():
 
     return parser.parse_args()
 
-def get_class(label_code):    
-    return CLASS_ls[label_code]
-
 # def read_img(path, image_ls):
 #     images_sample = random.shuffle(images)[:sample_num_per_class]
 
-
-def img_tag_Y_list(plant_class):
-    """
-    get img_name list w/ tag in eacg plant_class
-    """
-    img_tag_Y_df_dict = {}
-    img_tag_Y_list_dict = {}
-    img_tag_Y_list_df = img_tag_Y[img_tag_Y["Img"].isin(img_name_plant_class[plant_class])]
-    img_tag_Y_df_dict[plant_class] = img_tag_Y_list_df
-    img_tag_Y_list_dict[plant_class] = img_tag_Y_list_df["Img"].tolist()
-
-def crop_img_target(img_data, plant_class, img_name, img_shape, crop_length):
+def crop_img_target(img, plant_class, img_name, img_shape, crop_length):
     """
     get cropped img with raw img & target info
     """
-    img = img_data
     img_tag_Y_plant_df = img_tag_Y_df_dict[plant_class]
 
     ## targey info(準心資訊)
@@ -84,17 +68,20 @@ def crop_img_target(img_data, plant_class, img_name, img_shape, crop_length):
     crop_img = img[crop_w_lower:crop_w_upper, crop_h_lower:crop_h_upper]   
     return img_name, crop_img
 
+def crop_img():
+    pass
 
 def main():
     args = get_args()
 
+    # 資料讀取 >>>>>>>>>>>
+    os.makedirs(args.output_folder, exist_ok=True)
     class_detected_ls = os.listdir(args.file_folder)
-    class_folder_ls = [os.path.join(args.file_folder, i) for i in class_detected_ls]
+    class_folder_ls = [(i, os.path.join(args.file_folder, i)) for i in class_detected_ls]
 
+    coor_df = pd.read_csv('data/tag_locCoor.csv', encoding='big5', usecols=['TARGET_FID', 'Img', 'target_x', 'target_y'])
     with open(args.class_list_path, encoding='utf8') as f:
         CLASS_ls = [i.strip() for i in f.readlines()]
-    embed()
-    exit()
 
     # 檢查各類別資料夾存在
     absent_data = list(set(CLASS_ls) - set(class_detected_ls))
@@ -104,23 +91,36 @@ def main():
     not_used_data = list(set(class_detected_ls) - set(CLASS_ls))
     if len(not_used_data) != 0: 
         print('Category folders not used: ', not_used_data)
+    # <<<<<<<<<<<<<<<<<<<
 
 
-    for plant_class in plant_class_list:
-        img_tag_Y_list(plant_class)
-        
-    for k, v in img_tag_Y_list_dict.items():
-        print("{}:  Numbers of crop_img = {}".format(k, len(v)))
+    # 整理路徑、類別資訊到表上
+    print('searching image..')
+    image_info = []
+    for label, class_folder in tqdm.tqdm(class_folder_ls):
+        img_path_ls = glob.glob(f'{class_folder}/**/*.jpg', recursive=True)
+        img_ls = [os.path.basename(i) for i in img_path_ls]
+        label_ls = [label]*len(img_ls)
+        image_info.extend(tuple(zip(label_ls, img_ls, img_path_ls)))
+    
+    coor_df = coor_df.merge(
+        pd.DataFrame(image_info, columns=['label', 'Img', 'path']),
+        how='left', on='Img'
+        )
+    
+    coor_df['no_coor'] = (coor_df[['target_x', 'target_y']] == 0).all(axis=1)
 
-    Error_list = []
-    for plant_class, img_list in progress_bar(img_tag_Y_list_dict.items()):
-        for img_name in progress_bar(img_list):
-            img_data, img_shape = get_img_shape(plant_class, img_name)
-            try:
-                img_name_AA, crop_img_AA = crop_img_target(img_data, plant_class, img_name, img_shape_AA, 95)
-                save_crop_img(img_name_AA, crop_img_AA)
-            except Exception as ee:
-                Error_list.append(ee)
+    no_coor_ct = coor_df['no_coor'].value_counts()
+    print('image have coordinate: {}/{} ({:.2f}%)'.format(
+        no_coor_ct[False], 
+        no_coor_ct[True],
+        no_coor_ct[False]/no_coor_ct[True]
+        ))
+
+    coor_df.to_csv(os.path.join(args.output_folder, 'image_list.csv'), index=False)
+    embed()
+    exit()
+
 
 if __name__ == '__main__':
     main()
