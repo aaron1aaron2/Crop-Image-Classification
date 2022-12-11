3b9917c18d59502471beaa4d643de5f4f4dbd7ba
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-12-11 22:48:30 +0800

    Create  generate dataset batchfile

---
+++ scripts/generate_dataset/sample400_L200.bat
@@ -0,0 +1,11 @@
+python scripts/image_process.py ^
+    --file_folder data/predata ^
+    --class_list_path data/class_ls.txt ^
+    --img_coordinate_path data/tag_locCoor.csv ^
+    --output_folder data/sample400_L200 ^
+    --sample True ^
+    --sample_num_per_class 400 ^
+    --crop_length 200 ^
+    --train_ratio 0.7 ^
+    --val_ratio 0.1 ^
+    --test_ratio 0.2
