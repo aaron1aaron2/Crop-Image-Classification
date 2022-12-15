cdf82a037b45dd40e58c9a758d3023fe05b452be
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-12-15 19:42:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -12,6 +12,8 @@ GitHub: https://github.com/VincLee8188/GMAN-PyTorch
 Describe: train pipeline
 """
 import os
+import psutil
+import platform
 import argparse
 import torch
 import torch.nn as nn
@@ -29,15 +31,25 @@ from model.coatnet import CoAtNet
 def get_args():
     parser = argparse.ArgumentParser()
 
+    # 輸入
     parser.add_argument('--data_folder', type=str, default='data/sample200_L200')
 
-    parser.add_argument('--img_height', type=int, default=200)
-    parser.add_argument('--img_width', type=int, default=200)
+    # 輸出
+    parser.add_argument('--output_folder', type=str, default='./output/sample')
+    parser.add_argument('--use_Tracedmodule', type=str2bool, default=True)
 
-    parser.add_argument('--num_blocks', nargs='+', default=[2, 2, 12, 28, 2], help='Set num_blocks') # python arg.py -l 1234 2345 3456 4567
-    parser.add_argument('--channels', nargs='+', default=[64, 64, 128, 256, 512], help='Set channels') 
+    # 前處理
+    parser.add_argument('--img_height', type=int, default=224) # 32 倍數
+    parser.add_argument('--img_width', type=int, default=224) # 32 倍數
+    parser.add_argument('--img_nor_mean', type=float, nargs='+', default=(0.4914, 0.4822, 0.4465), help='mean in torchvision.transforms.Normalize')
+    parser.add_argument('--img_nor_std', type=float, nargs='+', default=(0.2023, 0.1994, 0.2010), help='std in torchvision.transforms.Normalize')
+
+    # coatnet 參數
+    parser.add_argument('--num_blocks', type=int, nargs='+', default=[2, 2, 12, 28, 2], help='Set num_blocks') # python arg.py -l 1234 2345 3456 4567
+    parser.add_argument('--channels', type=int, nargs='+', default=[64, 64, 128, 256, 512], help='Set channels') 
     parser.add_argument('--in_channels', type=int, default=3, help='Set in_channels') 
 
+    # 超參數
     parser.add_argument('--batch_size', type=int, default=24,
                         help='batch size')
     parser.add_argument('--val_batch_size', type=int, default=100,
@@ -51,13 +63,83 @@ def get_args():
     parser.add_argument('--decay_epoch', type=int, default=10,
                         help='decay epoch')
 
-    parser.add_argument('--output_folder', type=str, default='./output/sample')
+    # 其他
     parser.add_argument('--device', default='gpu', 
                         help='cpu or cuda')
 
     args = parser.parse_args()
 
     return args
+
+def check_args(args):
+    # 新增參數
+    args.device = 'cuda' if torch.cuda.is_available() and args.device in ['gpu', 'cuda'] else 'cpu'
+    args.train_folder = os.path.join(args.data_folder, 'train')
+    args.test_folder = os.path.join(args.data_folder, 'test')
+    args.val_folder = os.path.join(args.data_folder, 'val')
+
+    args.model_file = os.path.join(args.data_folder, 
+        'model.pt' if args.use_Tracedmodule else 'model.pkl'
+    )
+
+    # 檢查
+    for path in [args.train_folder, args.test_folder, args.val_folder]:
+        assert os.path.exists(path), f'Data not found at {path}'
+    assert len(args.num_blocks) == len(args.channels), '--num_blocks & --channels arguments must be of the same length'
+
+    # 路徑
+    args.fig_folder = os.path.join(args.output_folder, 'figure')
+
+    build_folder(args.output_folder)
+    # build_folder(fig_folder)
+
+    # config
+    saveJson(args.__dict__, os.path.join(args.output_folder, 'configures.json'))
+
+    return args
+
+def log_system_info(args, log):
+    message = f"Computer network name: {platform.node()}\n"+ \
+                f"Machine type: {platform.machine()}\n" + \
+                f"Processor type: {platform.processor()}\n" + \
+                f"Platform type: {platform.platform()}\n" + \
+                f"Number of physical cores: {psutil.cpu_count(logical=False)}\n" + \
+                f"Number of logical cores: {psutil.cpu_count(logical=True)}\n" + \
+                f"Max CPU frequency: {psutil.cpu_freq().max}\n"
+
+    cuda_divice = torch.cuda.get_device_name() if torch.cuda.is_available() else 'CPU'
+    message += f'Train with the {args.device}({cuda_divice})\n'
+    log_string(log, '='*20 + '\n' + message + '='*20)
+
+
+def load_data(args, log):
+    transform_train = transforms.Compose([
+        transforms.RandomRotation(90),
+        transforms.Resize((args.img_height, args.img_width)),
+        transforms.ToTensor(),
+        transforms.Normalize(args.img_nor_mean, args.img_nor_std),
+    ])
+
+    transform_eval = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(args.img_nor_mean, args.img_nor_std),
+    ])
+
+    train_folder = datasets.ImageFolder(args.train_folder, transform=transform_train)
+    val_folder = datasets.ImageFolder(args.val_folder, transform=transform_eval)
+    test_folder = datasets.ImageFolder(args.test_folder, transform=transform_eval)
+
+    log_string(log, f'\nclass idx:\n{train_folder.class_to_idx}\n')
+
+    train_loader = torch.utils.data.DataLoader(train_folder, batch_size=args.batch_size, shuffle=True, num_workers=0, pin_memory=True)
+    val_loader = torch.utils.data.DataLoader(val_folder, batch_size=args.val_batch_size, shuffle=False, num_workers=0)
+    test_loader = torch.utils.data.DataLoader(test_folder, batch_size=args.val_batch_size, shuffle=False, num_workers=0)
+
+    dataloaders_dict = {"train": train_loader, "val": val_loader, 'test': test_loader}
+
+    log_string(log, f'images numbers: train({len(train_folder)}) | val({len(val_folder)}) |test({len(test_folder)})')
+
+    return dataloaders_dict
 
 def train_model(log, model, dataloaders_dict, criterion, optimizer, scheduler, num_epochs, patience):
     best_acc = 0.0
@@ -66,7 +148,7 @@ def train_model(log, model, dataloaders_dict, criterion, optimizer, scheduler, n
         if wait >= patience:
             log_string(log, f'early stop at epoch: {epoch:04d}')
             break
-        model.cuda()
+
         for phase in ['train', 'val']:
             if phase == 'train':
                 model.train()
@@ -78,9 +160,10 @@ def train_model(log, model, dataloaders_dict, criterion, optimizer, scheduler, n
 
             dataloader = dataloaders_dict[phase]
             for item in tqdm(dataloader, leave=False):
-                images = item[0].cuda().float()
-                classes = item[1].cuda().long()
+                images = item[0].to(args.device).float()
+                classes = item[1].to(args.device).long()
                 optimizer.zero_grad()
+
                 with torch.set_grad_enabled(phase == 'train'):
                     output = model(images)
                     loss = criterion(output, classes)
@@ -97,84 +180,51 @@ def train_model(log, model, dataloaders_dict, criterion, optimizer, scheduler, n
             log_string(log, f'Epoch {epoch + 1}/{num_epochs} | {phase:^5} | Loss: {epoch_loss:.4f} | Acc: {epoch_acc:.4f}')
 
         scheduler.step()
+
         if epoch_acc > best_acc:
-            traced = torch.jit.trace(model.cpu(), torch.rand(1, 3, 224, 224))
-            traced.save(args.model_file)
-            # best_model_wts = model.state_dict()
+            best_model_wts = model.state_dict()
             log_string(log, f'val loss decrease from {best_acc:.4f} to {epoch_acc:.4f}, saving model to {args.model_file}')
 
             best_acc = epoch_acc
             wait = 0
         else:
             wait += 1
-    # model.load_state_dict(best_model_wts)
-    # torch.save(model, args.model_file)
+
+    model.load_state_dict(best_model_wts)
+
+    # 儲存模型
+    if args.use_Tracedmodule:
+        traced = torch.jit.trace(model.cpu(), torch.rand(1, 3, 224, 224))
+        traced.save(args.model_file)
+    else:
+        torch.save(model, args.model_file)
+
     log_string(log, f'Training and validation are completed, and model has been stored as {args.model_file}')
+
 
 if __name__ == '__main__':
     # 參數
     args = get_args()
-    args.device = 'cuda' if torch.cuda.is_available() and args.device in ['gpu', 'cuda'] else 'cpu'
-    args.num_blocks =[int(i) for i in args.num_blocks]
-    args.channels =[int(i) for i in args.channels]
-
-    # 路徑
-    fig_folder = os.path.join(args.output_folder, 'figure')
-
-    build_folder(args.output_folder)
-    build_folder(fig_folder)
-
-    # config
-    saveJson(args.__dict__, os.path.join(args.output_folder, 'configures.json'))
+    args = check_args(args)
 
     # log
     log = open(os.path.join(args.output_folder, 'log.txt'), 'w')
-    log_string(log, str(args)[10: -1])
-    log_string(log, f'main output folder{args.output_folder}')
+    log_string(log, f'{str(args)[10: -1]}\n')
+    log_string(log, f'main output folder: {args.output_folder}')
+    
+    log_system_info(args, log)
 
-    from IPython import embed
-    embed()
-    exit()
     # load data >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     log_string(log, 'loading data...')
-
-    transform_train = transforms.Compose([
-        transforms.RandomRotation(90),
-        transforms.Resize((224,224)),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-
-    train_data = datasets.ImageFolder('C:/pre', transform=transform_train)
-    val_data = datasets.ImageFolder('C:/pre', transform=transform_train)
-    test_data = datasets.ImageFolder('C:/pre', transform=transform_train)
-
-    log_string(log, f'{train_data.class_to_idx}')
-
-
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=0, pin_memory=True)
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=args.val_batch_size, shuffle=False, num_workers=0)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=args.val_batch_size, shuffle=False, num_workers=0)
-
-
-    dataloaders_dict = {"train": train_loader, "val": val_loader, 'test': test_loader}
-    # log_string(log, f'trainX: {trainX.shape}\t\t trainY: {trainY.shape}')
-    # log_string(log, f'valX:   {valX.shape}\t\tvalY:   {valY.shape}')
-    # log_string(log, f'testX:   {testX.shape}\t\ttestY:   {testY.shape}')
-    log_string(log, 'data loaded!')
-
-    
+    dataloaders_dict = load_data(args, log)
+    log_string(log, 'data loaded!\n')
     # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
     # build model >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     log_string(log, 'compiling model...')
-    # image_size, in_channels, num_blocks, channels, num_classes, block_types{'C': MBConv, 'T': Transformer}
-    model = CoAtNet((args.img_height, args.img_width), args.in_channels, args.num_blocks, args.channels, num_classes=33).to(args.device)
+    
+    model = CoAtNet((args.img_height, args.img_width), args.in_channels, args.num_blocks, args.channels, num_classes=33)
+    model = model.to(args.device)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
@@ -184,11 +234,13 @@ if __name__ == '__main__':
     
     parameters = count_parameters(model)
     log_string(log, 'trainable parameters: {:,}'.format(parameters))
+    log_string(log, 'model loaded!\n')
     # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
     # train model >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     log_string(log, 'training model...')
     train_model(log, model, dataloaders_dict, criterion, optimizer, scheduler, args.max_epoch, args.patience)
+    log_string(log, 'training finish\n')
     # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
     # test model >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
