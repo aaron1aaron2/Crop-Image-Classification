3e268dbea62726fc6a15540deccb4064730c66c8
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-12-25 17:24:21 +0800

    Update train_test-sample.bat

--- scripts/train_test-sample.bat
+++ scripts/train_test-sample.bat
@@ -1,11 +1,11 @@
 python train.py ^
     --data_folder data/sample10_L96(test) ^
-    --output_folder output/sample(test) ^
+    --output_folder output/sample(test)_ ^
     --img_height 96 ^
     --img_width 96 ^
     --batch_size 6 ^
     --val_batch_size 100 ^
-    --max_epoch 5 ^
+    --max_epoch 1 ^
     --learning_rate 0.01 ^
     --decay_epoch 2 ^
     --device gpu ^
