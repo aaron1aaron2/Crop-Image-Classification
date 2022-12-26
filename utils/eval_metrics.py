b0baa5b0a18043c05d5856477f0d0713d838bbeb
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-12-26 21:07:51 +0800

    Update eval_metrics.py

--- utils/eval_metrics.py
+++ utils/eval_metrics.py
@@ -3,9 +3,16 @@ Author: yen-nan ho
 Contact: aaron1aaron2@gmail.com
 Create Date: 2022.12.19
 Last Update: 2022.12.25
+
+[ISSUE]
+1. 在 sklearn 底下 許多 metrics 下 average 的使用
+    - binary: 只能用在二元資料下  
+    - Micro: 直接用全部資料的 true positives, false negatives and false positives 計算
+    - Macro: 直接加權平均
+    - weighted: 對每個 label 乘權重的加權平均(處理 unbalance data)
+    - samples: ??
 """
 import functools
-import numpy as np
 import pandas as pd
 
 from sklearn import metrics
@@ -15,7 +22,7 @@ from pretty_confusion_matrix import pp_matrix
 
 # y_true, y_score -> [0, 1, 2], [[0.5, 0.2, 0.2], [0.3, 0.4, 0.2], [0.2, 0.4, 0.3]]
 prob_metrics_dt = {
-    'Top_3_acc': functools.partial(metrics.top_k_accuracy_score, k=3), # y_true, y_pred(prob)
+    'Top_3_acc': functools.partial(metrics.top_k_accuracy_score, k=3),
     'Top_5_acc': functools.partial(metrics.top_k_accuracy_score, k=5),
     'Top_10_acc': functools.partial(metrics.top_k_accuracy_score, k=10),
     'roc_auc_score(ovr)': functools.partial(metrics.roc_auc_score, multi_class='ovr'),
@@ -24,12 +31,12 @@ prob_metrics_dt = {
 
 # y_true, y_pred -> [0, 1, 1, 0], [0, 1, 1, 1]
 metrics_dt = {
-    'Balanced_acc': metrics.balanced_accuracy_score, # y_true, y_pred)
+    'Balanced_acc': metrics.balanced_accuracy_score,
     'f1_micro': functools.partial(metrics.f1_score, average='micro'),
     'f1_macro': functools.partial(metrics.f1_score, average='macro'),
     'f1_weighted': functools.partial(metrics.f1_score, average='weighted'),
-    'report': functools.partial(metrics.classification_report, output_dict=True),
-    'confusion_matrix': metrics.confusion_matrix
+    # 'report': functools.partial(metrics.classification_report, output_dict=True),
+    # 'confusion_matrix': metrics.confusion_matrix
 }
 
 def plot_confusion_matrix(confusion_matrix, file_path, class_ls):
@@ -38,11 +45,14 @@ def plot_confusion_matrix(confusion_matrix, file_path, class_ls):
 
     plt.savefig(file_path)
 
-def Weighted_Precision(pred, target):
+def Weighted_Precision(pred, label):
+    metrics.recall_score()
+    metrics.precision_score()
     from IPython import embed
     embed() ; exit()
 
 def get_evaluation(pred, label, pred_prob):
     result_dt = {Weighted_Precision(pred, label)}
-    pass
+    
+    return report, result_dt
 
