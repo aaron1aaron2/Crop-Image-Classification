b73ff93039fb300f082e845873d65a5cf1c5c413
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-12-27 18:34:24 +0800

    Update train_test-sample.sh

--- scripts/train_test-sample.sh
+++ scripts/train_test-sample.sh
@@ -1,13 +1,11 @@
 python train.py \
-    --data_folder data/sample100_L160(test) \
-    --output_folder output/sample(test) \
+    --data_folder 'data/sample100_L160(test)' \
+    --output_folder 'output/sample(test)' \
     --img_height 160 \
     --img_width 160 \
-    --batch_size 6 \
+    --batch_size 32 \
     --val_batch_size 100 \
-    --max_epoch 10 \
+    --max_epoch 50 \
     --learning_rate 0.01 \
-    --decay_epoch 2 \
-    --device gpu \
-    --pin_memory_train True \
-    --prob True
+    --decay_epoch 10 \
+    --device gpu
