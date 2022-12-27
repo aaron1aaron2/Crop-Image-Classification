42cd019f2e0de24431ef25921ba6bb141f1a0c46
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-12-27 11:41:08 +0800

    Create train_test-sample.sh

---
+++ scripts/train_test-sample.sh
@@ -0,0 +1,13 @@
+python train.py \
+    --data_folder data/sample100_L160(test) \
+    --output_folder output/sample(test) \
+    --img_height 160 \
+    --img_width 160 \
+    --batch_size 6 \
+    --val_batch_size 100 \
+    --max_epoch 10 \
+    --learning_rate 0.01 \
+    --decay_epoch 2 \
+    --device gpu \
+    --pin_memory_train True \
+    --prob True
