c8ddb11d27a66a40e1f09fa7c3a34a41f77090e5
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-12-29 17:36:59 +0800

    Update check_image_size.py

--- scripts/check_image_size.py
+++ scripts/check_image_size.py
@@ -6,11 +6,12 @@ Last Update: 2022.12.29
 Describe: 檢查圖片大小
 """
 import os
-import argparse
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from PIL import Image 
 
-folder_images = "dat/predata"
+folder_images = "data/predata"
 size_images = {}
 
 for dirpath, _, filenames in os.walk(folder_images):
@@ -20,4 +21,14 @@ for dirpath, _, filenames in os.walk(folder_images):
             width, heigth = img.size
             size_images[path_image] = {'width': width, 'heigth': heigth}
 
+df = pd.DataFrame(size_images).T
+
+print('width:', df['width'].min()) # 690
+print('heigth:', df['heigth'].min()) # 690
+
+
+df['width'].plot.hist()
+df['heigth'].plot.hist()
+
+plt.show()
 print(size_images)
