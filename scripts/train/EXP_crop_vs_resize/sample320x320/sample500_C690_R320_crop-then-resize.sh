2dce362e7105c0137b77c92eca7d9d96c53669be
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-12-30 12:35:00 +0800

    Update scripts

---
+++ scripts/train/EXP_crop_vs_resize/sample320x320/sample500_C690_R320_crop-then-resize.sh
@@ -0,0 +1,13 @@
+# run on colab
+expname='EXP_crop_vs_resize/sample500_L690_crop-then-resize'
+python train.py \
+    --data_folder "data/${expname}" \
+    --output_folder "output/${expname}" \
+    --img_height 320 \
+    --img_width 320 \
+    --batch_size 8 \
+    --val_batch_size 64 \
+    --max_epoch 10 \
+    --learning_rate 0.01 \
+    --decay_epoch 3 \
+    --device gpu
