080433a0c232398f657b7f37be896a3fa751ffe0
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2023-01-01 12:34:59 +0800

    Create get_all_C690_R224.bat

---
+++ scripts/generate_dataset/get_all_C690_R224.bat
@@ -0,0 +1,13 @@
+python scripts/image_process.py ^
+    --file_folder data/predata ^
+    --class_list_path data/class_ls.txt ^
+    --img_coordinate_path data/tag_locCoor.csv ^
+    --output_folder data/all_C690_R224 ^
+    --sample False ^
+    --crop_image True ^
+    --crop_length 690 ^
+    --resize_image True ^
+    --resize_length 224 ^
+    --train_ratio 0.7 ^
+    --val_ratio 0.1 ^
+    --test_ratio 0.2
