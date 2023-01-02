5fa3a4856c572e1973638bc414ac29b4f74593c3
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2023-01-02 15:21:14 +0800

    Create SP2000_C690_R224.sh

---
+++ scripts/train/EXP_sample_nums/SP2000_C690_R224.sh
@@ -0,0 +1,15 @@
+# run on kaggle P100
+expname='EXP_sample_nums/SP2000_C690_R224'
+python train.py \
+    --data_folder "data/${expname}" \
+    --output_folder "output/${expname}" \
+    --img_height 224 \
+    --img_width 224 \
+    --batch_size 40 \
+    --val_batch_size 100 \
+    --max_epoch 25 \
+    --learning_rate 0.01 \
+    --decay_epoch 5 \
+    --decay_rate 0.5 \
+    --patience 5 \
+    --device tpu
