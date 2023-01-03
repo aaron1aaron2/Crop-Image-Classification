2183da3464604ad18525b8d3d41154cd4a2874d0
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2023-01-03 10:54:57 +0800

    Update scripts

---
+++ scripts/generate_dataset/EXP_crop_vs_resize.V2/all_C224_Rorg_crop.bat
@@ -0,0 +1,12 @@
+python scripts/image_process.py ^
+    --file_folder data/predata ^
+    --class_list_path data/class_ls.txt ^
+    --img_coordinate_path data/tag_locCoor.csv ^
+    --output_folder data/EXP_crop_vs_resize/sample500_L320_crop ^
+    --sample_file data/EXP_crop_vs_resize/sample500_Lorg_resize/image_info.csv ^
+    --crop_image True ^
+    --crop_length 320 ^
+    --resize_image False ^
+    --train_ratio 0.7 ^
+    --val_ratio 0.1 ^
+    --test_ratio 0.2
