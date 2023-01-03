46309483bb5b0c31f84070038e4a897d5ff852b9
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2023-01-03 19:07:32 +0800

    Update experiment scripts

--- scripts/train/EXP_crop_vs_resize.V2/sample500_Corg_R320_resize.sh
+++ scripts/train/EXP_crop_vs_resize.V2/sample500_Corg_R320_resize.sh
@@ -1,13 +1,14 @@
 # run on colab
-expname='EXP_crop_vs_resize/sample500_L480_crop'
+expname='EXP_crop_vs_resize/sample500_Lorg_resize'
 python train.py \
     --data_folder "data/${expname}" \
     --output_folder "output/${expname}" \
-    --img_height 480 \
-    --img_width 480 \
-    --batch_size 5 \
+    --img_height 320 \
+    --img_width 320 \
+    --batch_size 16 \
     --val_batch_size 64 \
     --max_epoch 10 \
     --learning_rate 0.01 \
     --decay_epoch 3 \
+    --decay_rate 0.5 \
     --device gpu
