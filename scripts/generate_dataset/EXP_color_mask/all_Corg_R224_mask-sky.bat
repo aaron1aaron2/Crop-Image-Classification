fad50ae57d5783654ffbfa2d5e051ebd8dd25d5e
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2023-01-07 21:12:04 +0800

    Create all_Corg_R224_mask-sky.bat

---
+++ scripts/generate_dataset/EXP_color_mask/all_Corg_R224_mask-sky.bat
@@ -0,0 +1,15 @@
+python scripts/image_process.py ^
+    --file_folder data/predata ^
+    --class_list_path data/class_ls.txt ^
+    --img_coordinate_path data/tag_locCoor.csv ^
+    --output_folder data/EXP_color_mask/all_Corg_R224_mask-sky ^
+    --sample_file data/EXP_crop_vs_resize.V2/all_Corg_R224/image_info.csv ^
+    --crop_image False ^
+    --resize_image True ^
+    --resize_length 224 ^
+    --mask_image True ^
+    --mask_lower 10 0 0 ^
+    --mask_upper 80 255 255 ^
+    --train_ratio 0.7 ^
+    --val_ratio 0.1 ^
+    --test_ratio 0.2
