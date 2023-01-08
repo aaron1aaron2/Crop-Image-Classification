2ad0d2ea93d47186870c17ea6562356a53165871
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2023-01-08 11:10:40 +0800

    Create all_Corg_R224_no-mask.sh

---
+++ scripts/train/EXP_color_mask/all_Corg_R224_no-mask.sh
@@ -0,0 +1,14 @@
+# run on kaggle P100
+expname='EXP_crop_vs_resize.V2/all_Corg_R224'
+python train.py \
+    --data_folder "data/${expname}" \
+    --output_folder "output/${expname}" \
+    --img_height 224 \
+    --img_width 224 \
+    --batch_size 40 \
+    --val_batch_size 100 \
+    --max_epoch 25 \
+    --learning_rate 0.01 \
+    --decay_epoch 5 \
+    --decay_rate 0.5 \
+    --device gpu
